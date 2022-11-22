961c66ffc6c907237363a2b073d2664269632643
aaaaaaalen <alenflynn@gmail.com>
2022-11-22 15:08:21 +1100

    finish the functionalities, but still need optimized the algorithm

--- nodeAPI.js
+++ nodeAPI.js
@@ -1,6 +1,45 @@
+
+const axios = require('axios')
 const arg = process.argv.slice(2);
  
 const category  = arg[0];
 const limit = Number(arg[1]);
+
+const getEntries = () => {
+    try {
+      return axios.get('https://api.publicapis.org/entries')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+const entriesList = async (category, limit) => {
+    const responseList = getEntries()
+        .then(response => {
+            if (response.data) {
+                const resultList = response.data.entries;
+                const matchList = [];
+                for(const i in resultList){
+                    if(resultList[i].Category === category){
+                        if(matchList.length <= limit){
+                            matchList.push(resultList[i]);
+                        }else{
+                            break;
+                        }
+                    }
+                }
+                if(matchList.length === 0){
+                    console.log('No results');  
+                }else{
+                    console.log(matchList);  
+                }
+                      
+            }
+        })
+        .catch(error => {
+        console.log(error)
+        })
+}
+
  
-console.log(`Category is ${category}, limit is ${limit}`);
+entriesList(category, limit)
